24528bdc5b07045a146a430b2d9df28c50ab2f8c
Mike Johnson Jr <75003652+mirojohn87@users.noreply.github.com>
2025-03-29 16:53:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-function toggleMenu() {
-  const menu = document.querySelector('.menu');
-  if (menu.style.right === '0px') {
-    menu.style.right = '-200px'; // Hide menu
-  } else {
-    menu.style.right = '0px'; // Show menu
-  }
-}
+// Example JavaScript functionality
+console.log("Page loaded successfully!");
