4a0b5ba301b5125731642775b676b6f105e4d393
Mike Johnson Jr <75003652+mirojohn87@users.noreply.github.com>
2025-05-03 14:07:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
- // Hamburger menu toggle function
-        function toggleMenu() {
-            const menu = document.getElementById("menu");
-            menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "block" : "none";
-        }
+// Hamburger menu toggle function
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "block" : "none";
+}
