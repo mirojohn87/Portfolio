10c150fae141d0c5e645f4f02834b799d11360a6
Mike Johnson Jr <75003652+mirojohn87@users.noreply.github.com>
2025-05-03 14:38:26 -0400

    Create Gal.js

---
+++ Gal.js
@@ -0,0 +1,49 @@
+class Gallery {
+    constructor(container) {
+        this.container = container;
+        this.gallery = container.querySelector('.gallery');
+        this.currentIndex = 0;
+        
+        // Sample images array - replace with your images
+        this.images = [
+            'image1.jpg',
+            'image2.jpg',
+            'image3.jpg'
+        ];
+        
+        this.init();
+        this.bindEvents();
+    }
+    
+    init() {
+        // Create and append images
+        this.images.forEach(src => {
+            const img = document.createElement('img');
+            img.src = src;
+            this.gallery.appendChild(img);
+        });
+    }
+    
+    bindEvents() {
+        const prev = this.container.querySelector('.prev');
+        const next = this.container.querySelector('.next');
+        
+        prev.addEventListener('click', () => this.slide('prev'));
+        next.addEventListener('click', () => this.slide('next'));
+    }
+    
+    slide(direction) {
+        if (direction === 'next') {
+            this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        } else {
+            this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+        }
+        
+        const offset = -this.currentIndex * 100;
+        this.gallery.style.transform = `translateX(${offset}%)`;
+    }
+}
+
+// Initialize the gallery
+const galleryContainer = document.querySelector('.gallery-container');
+new Gallery(galleryContainer);
